f492d5f153e666e74c45dd7f30d28499fdbafea7
onl7999 <213262728+onl7999@users.noreply.github.com>
2025-06-09 13:54:07 +0800

    Delete fn/db.php

--- fn/db.php
+++
@@ -1,6 +0,0 @@
-<?php
-$pdo = new PDO(
-    'mysql:host=localhost;dbname=osp;charset=utf8mb4',  
-    'root',                                             
-    '',  //passwd                                               
-);
